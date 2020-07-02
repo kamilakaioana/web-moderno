ef0498932083046df677ac66d7049c912171a98f
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2020-07-01 22:16:40 -0300

    comentários

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,18 @@
+// comentario de uma linha
+console.log('line 1');
+
+/*
+comentarios de 
+múltiplas linhas
+*/
+console.log('line 2');
+
+/*
+ * comentário de
+ * multiplas linhas
+ * 
+ */
+console.log('line 3');
+
+ 
+
