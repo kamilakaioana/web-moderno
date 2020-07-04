99a6559ef894397c143544103be383219e996c96
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2020-07-03 23:23:29 -0300

    Numeros -math -strings

---
+++ fundamentos/strings.js
@@ -0,0 +1,20 @@
+const school = "Cod3r";
+
+console.log(school.charAt(4));
+console.log(school.charAt(5));
+console.log(school.charCodeAt(3));
+console.log(school.indexOf('3'));
+
+console.log(school.substring(1));
+console.log(school.substring(0, 3));
+
+console.log('School '.concat(school).concat("!"));
+console.log('School ' + school + "!");
+console.log(school.replace(3, 'e'));
+
+
+console.log('Ana,Maria,Pedro'.split(','));
+
+
+
+
