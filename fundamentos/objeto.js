24d4c33f6afdcba2f7bf4643dd384859334448f0
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2020-07-05 23:41:32 -0300

    class 24 (object)

---
+++ fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {};
+prod1.name = 'Cell phone';
+prod1.price = 4998.90;
+prod1['Cool discount'] = 0.40; //evitar atributos com espaço
+
+console.log(prod1);
+
+const prod2 = {
+    name: 'Polo shirt',
+    price: 79.90
+}
+  
+console.log(prod2)
