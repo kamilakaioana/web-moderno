eee44170e6b00d59767c26326fd0cb976806cced
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2020-07-07 22:35:14 -0300

    Class 29 (Var)

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,20 @@
+{
+    { 
+        {
+            {
+                var exist = 'exist??';
+                console.log(exist);
+            } 
+        }
+    }
+}
+
+console.log(exist);
+
+function test() {
+    var local = 123;
+    console.log(local);
+}
+
+//test()
+//console.log(local)
