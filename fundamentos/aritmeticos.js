ac6f82be6877125530f587a6cd9e836fc314a0f6
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2020-07-13 22:40:49 -0300

    class 45

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const sum = a + b + c + d;
+const subtraction = d - b;
+const multiplication = a * b;
+const division = d / a;
+const modulo = a % 2;
+
+console.log(sum, subtraction, division, modulo, multiplication);
