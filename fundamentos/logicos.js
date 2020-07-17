09a26bc14defc5c77688b18bc13ecacd86099f7a
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2020-07-16 22:30:45 -0300

    class

--- fundamentos/logicos.js
+++ fundamentos/logicos.js
@@ -1,5 +1,14 @@
-function compras(work1, work2) {
+function buy(work1, work2) {
     const buyIceCream = work1 || work2;
     const buyTv50 = work1 && work2;
-    
+    // const buyTv32 = !!(work1 ^ work2); // bitwise
+    const buyTv32 = work1 != work2;
+    const keepheathy = !buyIceCream; // operador unário
+
+    return { buyIceCream, buyTv50, buyTv32, keepheathy };  
 }
+
+console.log(buy(true, true));
+console.log(buy(true, false));
+console.log(buy(false, true));
+console.log(buy(false, false));
